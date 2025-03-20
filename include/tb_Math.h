4ade15bbd5b23de10f941324125696eb4b803856
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-03-20 12:06:25 -0700

    Add helper functions and add clang-format

--- include/tb_Math.h
+++ include/tb_Math.h
@@ -3,12 +3,22 @@
 
 namespace tb {
 
-template <typename T>
+template<typename T>
+[[nodiscard]] bool inRange(T value, T min, T max) {
+    return value >= min && value <= max;
+}
+
+template<typename T>
+[[nodiscard]] bool is0to1(T value) {
+    return inRange(value, static_cast<T>(0), static_cast<T>(1));
+}
+
+template<typename T>
 [[nodiscard]] T to0to1(T value, T inMin, T inMax) {
-  assert(value >= inMin && value <= inMax);
-  value = (value - inMin) / (inMax - inMin);
-  assert(value >= static_cast<T>(0) && value <= static_cast<T>(1));
-  return (value - inMin) / (inMax - inMin);
+    assert(inRange(value, inMin, inMax));
+    value = (value - inMin) / (inMax - inMin);
+    assert(is0to1(value));
+    return value;
 }
 
 }
