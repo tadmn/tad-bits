ddde9032a1eebad13a02e38beae0291814733219
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-04-08 11:08:31 -0700

    Switch to using std::numbers::pi instead of M_PI

--- include/tb_Windowing.h
+++ include/tb_Windowing.h
@@ -3,7 +3,7 @@
 
 #include <cmath>
 #include <vector>
-#include <concepts>
+#include <numbers>
 
 namespace tb {
 
@@ -15,11 +15,13 @@ enum class WindowType {
 
 template <std::floating_point T>
 std::vector<T> window(WindowType windowType, int size) {
+    constexpr auto pi = std::numbers::pi;
+
     std::vector<T> window(size, 0);
 
     if (windowType == WindowType::Hann) {
         for (size_t i = 0; i < window.size(); ++i)
-            window[i] = static_cast<T>(0.5 - std::cos((2.0 * i * M_PI) / (window.size() - 1)) / 2);
+            window[i] = static_cast<T>(0.5 - std::cos((2.0 * i * pi) / (window.size() - 1)) / 2);
     } else if (windowType == WindowType::BlackmanHarris) {
         constexpr T a0 = 0.35875;
         constexpr T a1 = 0.48829;
@@ -28,12 +30,12 @@ std::vector<T> window(WindowType windowType, int size) {
 
         for (size_t i = 0; i < window.size(); ++i) {
             const auto x = i / static_cast<double>(window.size() - 1);
-            window[i] = static_cast<T>(a0 - a1 * std::cos(2.0 * M_PI * x) + a2 * std::cos(4.0 * M_PI * x) -
-                        a3 * std::cos(6.0 * M_PI * x));
+            window[i] = static_cast<T>(a0 - a1 * std::cos(2.0 * pi * x) + a2 * std::cos(4.0 * pi * x) -
+                        a3 * std::cos(6.0 * pi * x));
         }
     } else if (windowType == WindowType::Hamming) {
         for (size_t i = 0; i < window.size(); ++i)
-            window[i] = static_cast<T>(0.54 - 0.46 * std::cos((2.0 * i * M_PI) / (window.size() - 1)));
+            window[i] = static_cast<T>(0.54 - 0.46 * std::cos((2.0 * i * pi) / (window.size() - 1)));
     } else {
         assert(false);
     }
