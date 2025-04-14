72d3efe6fc5e32bb09a099a81de78eec76f211e4
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2025-04-14 16:32:58 -0700

    Add dsp utilities

---
+++ include/tb_DspUtilities.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+#include <choc_SampleBuffers.h>
+#include <numbers>
+
+namespace tb {
+
+// Creates a sine wave buffer with the specified parameters
+inline choc::buffer::ChannelArrayBuffer<float> makeSineWave(float frequency, double sampleRate,
+                                                            int numChannels, int numSamples,
+                                                            float amplitude = 1.f) {
+    auto buffer = choc::buffer::ChannelArrayBuffer<float>(numChannels, numSamples);
+
+    // Fill all channels with the same sine wave
+    choc::buffer::setAllSamples(buffer, [=](int channel, int frame) {
+        return amplitude * std::sin(2.0 * std::numbers::pi * frequency * frame / sampleRate);
+    });
+
+    return buffer;
+}
+
+}
