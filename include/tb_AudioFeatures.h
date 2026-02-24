0a673c048fe4dfbffad150028cd4be3a2a306b82
Tad Nicol <34101867+tadmn@users.noreply.github.com>
2026-02-24 13:31:31 -0800

    AudioFeatures: Use power spectrum in spectral centroid

--- include/tb_AudioFeatures.h
+++ include/tb_AudioFeatures.h
@@ -4,17 +4,18 @@
 
 namespace tb {
 
-// Expects one-sided FFT magnitudes [0, N/2] inclusive, where N is the FFT size.
-static float spectralCentroid(std::span<const float> fftMagnitudes, double sampleRate) {
-    const double freqResolution = sampleRate / (2.0 * (fftMagnitudes.size() - 1));
+// Expects one-sided FFT power spectrum values [0, N/2] inclusive, where N is the FFT size.
+// Values should be magnitude squared of raw fft complex output.
+static float spectralCentroid(std::span<const float> fftPowerSpectrum, double sampleRate) {
+    const double freqResolution = sampleRate / (2.0 * (fftPowerSpectrum.size() - 1));
 
     double weightedSum = 0.0;
     double magnitudeSum = 0.0;
 
-    for (size_t i = 0; i < fftMagnitudes.size(); ++i) {
+    for (size_t i = 0; i < fftPowerSpectrum.size(); ++i) {
         double frequency = i * freqResolution;
-        weightedSum += frequency * fftMagnitudes[i];
-        magnitudeSum += fftMagnitudes[i];
+        weightedSum += frequency * fftPowerSpectrum[i];
+        magnitudeSum += fftPowerSpectrum[i];
     }
 
     double centroid = 0.0;
